04d34a705c29668c89a9d17578a54b2db3096f27
taoliu12 <taoliudesign@gmail.com>
2019-08-28 11:52:52 -0700

    add pokemon list

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,14 +2,23 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
-//fetch trainers
-//createelements: div, button, ul, li
-
 function renderTrainers(trainers) {
     trainers.forEach(trainer => {
         const trainerCard = generateTrainerCard(trainer)         
         document.querySelector('main').appendChild(trainerCard)
     })
+}
+
+function generatePokemonLi(pokemon) {
+    const li = document.createElement('li')
+    li.innerText = pokemon.nickname    
+    
+    const releaseButton = document.createElement('button')
+    releaseButton.className = 'release'
+    releaseButton.innerHTML = 'Release'
+    li.appendChild(releaseButton)
+
+    return li
 }
 
 function generateTrainerCard(trainer) {
@@ -23,18 +32,14 @@ function generateTrainerCard(trainer) {
         const addButton = document.createElement('button')
         addButton.innerHTML = 'Add Pokemon'
         trainerCard.appendChild(addButton)
-
-        const releaseButton = document.createElement('button')
-        releaseButton.className = 'release'
-        releaseButton.innerHTML = 'Release'
-
+ 
         const ul = document.createElement('ul')
-        const li = document.createElement('li')
-        li.innerText = 'poke'
-        li.appendChild(releaseButton)
 
-        ul.appendChild(li)
-    
+        trainer.pokemons.forEach(pokemon => {
+            const li = generatePokemonLi(pokemon)
+            ul.appendChild(li)
+        });
+      
         trainerCard.appendChild(ul)
         return trainerCard
 }
@@ -43,6 +48,6 @@ fetch(TRAINERS_URL)
 .then(response => response.json())
 .then(trainers => {          
         renderTrainers(trainers)
-    });
+});
 
 
