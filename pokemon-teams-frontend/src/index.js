cc3422d8b596a7c3399c7761b9d2ec3235cdecfe
taoliu12 <taoliudesign@gmail.com>
2019-08-27 23:26:56 -0700

    add fetch trainers

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,10 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+//fetch trainers
+//createelements: div, button, ul, li
+
+fetch(TRAINERS_URL)
+.then(response => response.json())
+.then(json => console.log(json))
