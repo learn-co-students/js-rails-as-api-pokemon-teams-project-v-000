db3f763410d53801172bde441bc28238cb13ec46
tmetz <tammy@tammymetz.com>
2019-10-13 17:09:01 -0700

    add index action

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,29 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+function fetchTrainers() {
+    return fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+    .then(json => renderTrainers(json))
+}
+
+function renderTrainers(json) {
+    const main = document.querySelector('main')
+    json.forEach(trainer => {
+        const h2 = document.createElement('h2')
+        h2.innerHTML = `<h2>${trainer.name}</h2>`
+        main.appendChild(h2)
+        const ul = document.createElement('ul')
+        trainer.pokemons.forEach(pokemon => {
+            const li = document.createElement('li')
+            li.innerHTML = `${pokemon.nickname}`
+            ul.appendChild(li)
+        })
+        main.appendChild(ul)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchTrainers()
+})
