0e367df72d2d503cd3dd5aaa48a9122772478f80
epic0448 <samuelogundiran@Yahoo.com>
2019-10-14 11:21:45 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,3 +3,19 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 //   Use the Asynchrony to finish out this project.
+
+const add_pokemon = document.getElementById('add_pokemons');
+    add_pokemon.addEventListener('click', function (event){
+        event.preventDefault()
+      fetch(POKEMONS_URL)
+
+    })
+
+
+// fetch(BASE_URL)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(json) {
+//         console.log(json)
+//     });
