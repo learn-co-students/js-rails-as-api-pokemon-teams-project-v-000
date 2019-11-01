149341c92fe7bc2a5466310399e6d1c409bb7c41
AlwinaO <aoyewole2586@gmail.com>
2019-11-01 18:35:18 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchTrainers();
+})
+
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+// fetch all trainers and append to div.card 
+// then loop over each trainer's list of pokemon and display
+
+const fetchTrainers = () => {
+
+    fetch(TRAINERS_URL)
+    .then((response) => response.text())
+    .then((data) => console.log(data, "trainers found")) 
+}
