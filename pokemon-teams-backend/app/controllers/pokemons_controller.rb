bbee8b2ce7bc57587b91b1528fc803b72285497d
AlwinaO <aoyewole2586@gmail.com>
2019-11-01 12:10:04 -0400

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,12 @@
 class PokemonsController < ApplicationController
+
+    def index
+        pokemons = Pokemon.all
+        render json: pokemons, only: [:id, :species, :nickname, :trainer_id]
+    end
+
+    def show
+        pokemon = Pokemon.find(params[:id])
+        render json: pokemon, only: [:id, :species, :nickname, :trainer_id]
+    end
 end
--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,12 @@
 class TrainersController < ApplicationController
+
+    def index
+        trainers = Trainer.all
+        render json: trainers, only: [:id, :name]
+    end
+    
+    def show
+        trainer = Trainer.find(params[:id])
+        render json: trainer, only: [:id, :name]
+    end
 end
