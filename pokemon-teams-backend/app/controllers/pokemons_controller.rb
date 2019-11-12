4bef96f9b2649cb413c35e30c28ee8907facde66
Rebecca Tong <rtong@buffalo.edu>
2019-11-11 20:53:33 -0500

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,3 +1,17 @@
 class PokemonsController < ApplicationController
   #new pokemon, and release pokemon
+
+  def create
+    new_pokemon = Pokemon.new(params)
+  end
+
+  def show
+    pokemon = Pokemon.find_by(id: params[:id])
+  end
+
+  def index
+    pokemons = Pokemon.all
+    render json: pokemons
+  end
+  
 end
--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,3 +1,8 @@
 class TrainersController < ApplicationController
   #see all trainers on current team of pokemon
+
+  def show
+    trainer = Trainer.find_by(id: params[:id])
+    render json: trainer, include: [:name]
+  end
 end
