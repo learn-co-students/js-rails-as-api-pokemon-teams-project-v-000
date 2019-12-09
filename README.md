aba2550fc1a0ff9924ac8e04408dea35b85fc703
github username <github email address>
2019-12-09 15:26:49 -0500

    Done.

--- README.md
+++ README.md
@@ -60,7 +60,7 @@ build, add the following gem to the Gemfile:
 gem 'faker'
 ```
 
-While we are in the Gemfile, uncomment `gem 'rack-cors'`, then run 
+While we are in the Gemfile, uncomment `gem 'rack-cors'`, then run
 `bundle install`. If you recall, `rack-cors` is necessary for cross-origin
 resource sharing. To get rack-cors working, once the gem is installed, uncomment
 the following code in `config/initializers/cors.rb`:
@@ -84,7 +84,7 @@ cross-origin HTTP requests.
 
 The string after `origins` specifies which hosts will be allowed to make
 requests to your API. Your brand-new rails app probably comes with the string
-`"example.com"` there — change it to "*" to allow all hosts. In a production
+`"example.com"` there — change it to * to allow all hosts. In a production
 application, you might want to specify a particular hostname.
 
 If you already have a rails server running, stop and restart it so that your
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,96 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+const main = document.querySelector('main');
+
+function renderPokemon(data, li) {
+  li.innerHTML = `${data.species} (${data.nickname}) <button class="release" data-pokemon-id="${data.id}">Release</button>`;
+}
+
+function addPokemon(event) {
+  let pokemon_list = event.currentTarget.nextElementSibling
+  if (pokemon_list.childElementCount < 6) {
+    let data = {
+      "trainer_id": event.currentTarget.getAttribute("data-trainer-id")
+    }
+
+    let new_li = document.createElement('li');
+
+    fetch(POKEMONS_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(resp => resp.json())
+    .then(json => renderPokemon(json["data"].attributes, new_li))
+
+    pokemon_list.appendChild(new_li);
+  } else {
+    alert('Sorry, trainers are only allowed 6 pokemon.')
+  }
+}
+
+function fetchTrainers() {
+  fetch(TRAINERS_URL)
+    .then(resp => {
+      return resp.json()
+    })
+    .then(json => {
+      renderTrainers(json["data"])
+    });
+};
+
+function renderTrainers(data) {
+  data.forEach(trainer => {
+    //create pokemons variable
+    const pokemons = trainer["relationships"]["pokemons"]["data"]
+    // create div container
+    const div = document.createElement('div');
+    div.setAttribute('class', 'card');
+    div.setAttribute('data-id', `${trainer.id}`);
+    div.innerHTML = `<p>${trainer["attributes"]["name"]}</p>`;
+
+    // create add pokemon button
+    const button = document.createElement('button');
+    button.setAttribute('data-trainer-id', `${trainer.id}`);
+    button.innerHTML = 'Add Pokemon';
+    button.addEventListener('click', addPokemon);
+
+    div.appendChild(button);
+    // generate pokemon list container
+    const ul = document.createElement('ul');
+
+    pokemons.forEach(function(pokemon) {
+      const li = document.createElement('li');
+
+      fetch(`${POKEMONS_URL}/${pokemon.id}`)
+        .then(resp => {
+          return resp.json()
+        })
+        .then(json => {
+          renderPokemon(json["data"].attributes, li);
+        })
+      ul.appendChild(li);
+   });
+   div.appendChild(ul);
+   main.appendChild(div);
+
+  })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchTrainers();
+
+  main.addEventListener('click', function(event) {
+    if(event.target.nodeName === "BUTTON") {
+      // make previous if statement with && if you can generate add pokemon buttons with event listeners
+      if(event.target.className === "release") {
+        if(confirm('Are you sure you want to release?')) {
+          event.target.parentNode.remove();
+        }
+      }
+    }
+  });
+});
