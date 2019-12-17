49585be1979efd9ea2decdf454301218b239f187
Jason Mei <jason.mei403@gmail.com>
2019-12-17 16:20:11 -0500

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -3,4 +3,31 @@ class PokemonsController < ApplicationController
     pokemons = Pokemon.all
     render json: pokemons
   end
+
+  def show
+    pokemon = Pokemon.find(params[:id])
+    render json: pokemon
+  end
+
+  def create
+    pokemon = generatePokemon(params[:trainer_id])
+    render json: pokemon
+  end
+
+  def destroy
+    pokemon = Pokemon.find(params[:id])
+    pokemon.delete
+    render json: pokemon
+  end
+
+  private
+  #   def pokemon_params
+  #     params.require(:pokemon).permit(:species, :nickname, :trainer_id)
+  #   end
+
+  def generatePokemon(trainer_id)
+    name = Faker::Name.first_name
+    species = Faker::Games::Pokemon.name
+    Pokemon.create(nickname: name, species: species, trainer_id: trainer_id)
+  end
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,10 +3,10 @@ const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 const trainerCollection = document.querySelector('#trainer-collection');
 
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', () => {
   getTrainers().then(trainers => {
     trainers.forEach(trainer => {
-      renderTrainers(trainer);
+      renderTrainer(trainer);
     })
   })
 });
@@ -16,27 +16,82 @@ function getTrainers() {
   .then(res => res.json());
 }
 
-function renderTrainers(trainer) {
+function renderTrainer(trainer) {
   const p = document.createElement('p');
   p.innerText = trainer.name;
 
   const addBtn = document.createElement('button');
-  addBtn.setAttribute('class', 'add');
+  addBtn.className = 'add';
+  addBtn.setAttribute('data-trainer-id', trainer.id);
   addBtn.innerText = "Add Pokemon";
+  addBtn.addEventListener('click', e => {
+    if (trainer.pokemons.length < 6) {
+      fetch(POKEMONS_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          trainer_id: e.target.dataset.trainerId
+        })
+      })
+      .then(resp => resp.json())
+      .then(pokemon => {
+        trainer.pokemons.push(pokemon);
+        const li = renderPokemon(pokemon);
+        const releaseBtn = renderReleaseBtn(pokemon);
+
+        li.appendChild(releaseBtn);
+        ul.appendChild(li);
+      })
+    } else {
+      alert(`${trainer.name}'s party is full!`)
+    }
+  })
 
   const ul = document.createElement('ul');
   trainer.pokemons.forEach(pokemon => {
-    const li = document.createElement('li');
-    li.innerText = `${pokemon.nickname} (${pokemon.species})`
-    const releaseBtn = document.createElement('button');
-    releaseBtn.setAttribute('class', 'release');
-    releaseBtn.innerText = "Release";
+    const li = renderPokemon(pokemon)
+    const releaseBtn = renderReleaseBtn(pokemon)
+
     li.appendChild(releaseBtn);
     ul.appendChild(li);
   });
 
   const divCard = document.createElement('div');
-  divCard.setAttribute('class', 'card');
+  divCard.className = 'card';
+  divCard.setAttribute('data-id', trainer.id);
   divCard.append(p, addBtn, ul);
   trainerCollection.append(divCard);
+}
+
+function renderPokemon(pokemon) {
+  const li = document.createElement('li');
+  li.innerText = `${pokemon.nickname} (${pokemon.species})`;
+  return li;
+}
+
+function renderReleaseBtn(pokemon) {
+  const releaseBtn = document.createElement('button');
+  releaseBtn.className = 'release';
+  releaseBtn.setAttribute('data-pokemon-id', pokemon.id);
+  releaseBtn.innerText = "Release";
+  releaseBtn.addEventListener('click', e => {
+    fetch(POKEMONS_URL + `/${pokemon.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        trainer_id: e.target.dataset.trainerId
+      })
+    })
+    .then(resp => resp.json())
+    .then(pokemon => {
+      document.querySelector(`[data-pokemon-id="${pokemon.id}"]`).parentNode.remove()
+    })
+  })
+  return releaseBtn;
 }
