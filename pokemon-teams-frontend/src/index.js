0a6a4bd4e540f63f89f322b67731fe6f2073c4c4
Shaun McLoughlin <skmcloughlin@gmail.com>
2019-12-24 08:21:09 -0500

    CORS working

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,33 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+const BASE_URL = "http://localhost:3000";
+const TRAINERS_URL = `${BASE_URL}/trainers`;
+const POKEMONS_URL = `${BASE_URL}/pokemons`;
+
+// DESIRED HTML
+/* <div class="card">
+<p>Coach name</p>
+<button>Add Pokemon</button>
+<ul>
+  <li>
+    Pokemon Name
+    <button class="release">Release</button>
+  </li>
+</ul>
+</div> */
+
+// const addButton = document.getElementById("add");
+// addButton.addEventListener("click", addPokemon);
+
+// function addPokemon() {
+//   console.log("addPokemon called");
+// }
+
+const trainersResponse = fetch(TRAINERS_URL);
+// const trainers = await trainersResponse.json();
+
+// trainers.data.forEach(trainer => {
+//     def renderTrainerCard() {
+
+//     };
+//     renderTrainerCard(trainer)
+//   console.log(trainer.attributes.name);
+// });
