06d376ef110400c3dc5cfb79d9874f1b1f51c7c9
Heather <heather.miller114@gmail.com>
2020-01-29 18:24:59 -0500

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -11,9 +11,18 @@ class PokemonsController < ApplicationController
     end
 
     def create
+        trainer = Trainer.find_by(id: params[:trainer_id])
+        name = Faker::Name.first_name
+        species = Faker::Games::Pokemon.name
+        pokemon = Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
+        render json: PokemonSerializer.new(pokemon)
     end
 
     def destroy
+        pokemon = Pokemon.find_by(id: params[:id])
+        pokemon.destroy
+
+        render json: PokemonSerializer.new(pokemon)
     end
 
 end
