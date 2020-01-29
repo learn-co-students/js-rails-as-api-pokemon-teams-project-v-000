0e6949eaded717d6f557f33198064878f06e6928
Heather <heather.miller114@gmail.com>
2020-01-29 16:02:31 -0500

    trainers#show with json

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,7 @@
 class TrainersController < ApplicationController
+
+    def show
+        trainer = Trainer.find_by(id: params[:id])
+        render json: trainer, include: [:pokemons]
+    end
 end
