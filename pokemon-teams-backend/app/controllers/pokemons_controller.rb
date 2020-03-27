2364d8898dd2fed141d9ba49dc7a79b28e115048
Meg Gutshall <meghan.gutshall@gmail.com>
2020-03-27 01:09:13 -0400

    Add validation  and error message to pokemon create action

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -2,9 +2,14 @@ require 'faker'
 
 class PokemonsController < ApplicationController
   def create
-    trainer = Trainer.find_by(params[:id])
-    pokemon = trainer.pokemons.create(species: Faker::Games::Pokemon.name, nickname: Faker::Name.first_name)
-    redirect_to trainers_path
+    trainer = Trainer.find_by(id: params[:trainer_id])
+    if trainer.pokemons.count >= 6
+      trainer.errors[:base] << "Looks like your roster is full! Please release a Pokemon before adding another to your team."
+      render json: { :errors => trainer.errors[:base] }
+    else
+      pokemon = trainer.pokemons.create(species: Faker::Games::Pokemon.name, nickname: Faker::Name.first_name)
+      render json: pokemon
+    end
   end
 
   def destroy
