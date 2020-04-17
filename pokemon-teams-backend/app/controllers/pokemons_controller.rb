656c48844806df1247c72448a52278172ba4f718
Jax05 <carley.tripp@outlook.com>
2020-04-17 18:44:20 -0500

    Update backend API

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,5 @@
+class PokemonsController < ApplicationController
+  # name = Faker::Name.first_name
+  # species = Faker::Games::Pokemon.name
+  # Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
+end
