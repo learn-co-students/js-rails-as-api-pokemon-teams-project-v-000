c0d996d76e29c458b049861bccf607f728f478b3
Jax05 <carley.tripp@outlook.com>
2020-04-19 19:16:33 -0500

    Add Pokemon routes JSON response

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -2,4 +2,19 @@ class PokemonsController < ApplicationController
   # name = Faker::Name.first_name
   # species = Faker::Games::Pokemon.name
   # Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
+  def index
+    pokemons = Pokemon.all
+    render json: pokemons, except: [:created_at, :updated_at]
+  end
+
+  def create
+    nickname = Faker::Name.first_name
+    species = Faker::Games::Pokemon.name
+    Pokemon.create(nickname: nickname, species: species)
+  end
+
+  def destroy
+    pokemon = Pokemon.find_by(id: params[:id])
+    pokemon.destroy
+  end
 end
