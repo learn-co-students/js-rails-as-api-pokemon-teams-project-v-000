3a297d495a5e04e0ff0a1670905cf7eb842e9194
Jax05 <carley.tripp@outlook.com>
2020-04-19 20:30:40 -0500

    Tidy up controller

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,16 +1,19 @@
+require "faker"
+
 class PokemonsController < ApplicationController
-  # name = Faker::Name.first_name
-  # species = Faker::Games::Pokemon.name
-  # Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
   def index
     pokemons = Pokemon.all
     render json: pokemons, except: [:created_at, :updated_at]
   end
 
   def create
+    trainer = Trainer.find_by(id: params[:trainer_id])
     nickname = Faker::Name.first_name
     species = Faker::Games::Pokemon.name
-    Pokemon.create(nickname: nickname, species: species)
+
+    if trainer && trainer.pokemons.count < 6
+      pokemon = trainer.pokemons.create(nickname: nickname, species: species, trainer_id: trainer.id)
+    end
   end
 
   def destroy
