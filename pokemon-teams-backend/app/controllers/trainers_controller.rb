ba776afd61b12859169dcf76c3c915c723233a17
sjwakeman <swakeman@ca.rr.com>
2020-05-05 21:35:23 -0700

    Done.

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,16 @@
 class TrainersController < ApplicationController
+    def index
+        trainers = Trainer.all
+        render json: TrainerSerializer.new(trainers)
+    end
+
+    def show
+        trainer = Trainer.find_by(id: params[:id])
+        options = {
+          include: [:name]
+        }
+        render json: TrainerSerializer.new(trainer, options)
+      end
+    
+       
 end
