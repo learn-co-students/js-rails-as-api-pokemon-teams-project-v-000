294246148e83d22ae55c8b4d83dcbb4d72c4e4e1
sjwakeman <swakeman@ca.rr.com>
2020-05-10 20:10:11 -0700

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,7 +1,6 @@
 class PokemonsController < ApplicationController
     def index
         pokemons = Pokemon.all
-        # render json: PokemonSerializer.new(pokemons)#.to_serialized_json #DID NOT WORK
         render json: pokemons
     end
 
@@ -10,8 +9,15 @@ class PokemonsController < ApplicationController
         options = {
             include: [:trainer]
         }
-        # render json: PokemonSerializer.new(pokemon)#.to_serialized_json #DID NOT WORK
-        # render json: PokemonSerializer.new(pokemon, options) #DID NOT WORK
         render json: pokemon
     end
+
+    def create
+        trainer = Trainer.find(params[:trainer_id])
+    end
+
+    def destroy
+
+    end
+
 end
