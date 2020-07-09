77068ab508308c85c21874f41d9b8468094dde3b
github username <github email address>
2020-07-09 14:29:17 -0400

    Fetch all trainers and their pokemons

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,13 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+document.addEventListener("DOMContentLoaded", fetchData);
+
+function fetchData() {
+    console.log("fetchData");
+    fetch(TRAINERS_URL)
+        .then(resp => resp.json())
+        .then(json => console.log(json))
+        .catch(error => console.log(`My Error: ${error}`));
+
+}
