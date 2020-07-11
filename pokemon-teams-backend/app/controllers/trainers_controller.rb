ddbc9b12540d501dad6d7954e9bebcd236ff3482
Eva Navarro <emanao@me.com>
2020-07-11 22:09:37 +0000

    Done

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -17,24 +17,24 @@ class TrainersController < ApplicationController
         if(trainer && trainer.pokemons.size<6)
             new_pokemon = trainer.pokemons.create(nickname: Faker::Name.first_name, species: Faker::Games::Pokemon.name, trainer_id: trainer.id)
 
-            render json: new_pokemon,except:[:created_at, :updated_at]            
+            render json: new_pokemon,except:[:created_at, :updated_at]
         else
-            render json: {message: "Reached max. number of pokemons per trainer."}  
+            render json: {message: "Reached max. number of pokemons per trainer."}
         end
     end
-    def delete 
+    def delete
         # byebug
         trainer = Trainer.find_by(id: params[:trainer_id])
         if trainer
             pokemon = trainer.pokemons.find_by(id: params[:id])
             if pokemon.nil?
-                render json: {message: "Pokemon not found!"}  
+                render json: {message: "Pokemon not found!"}
             else
                 pokemon.delete
                 render json: pokemon,except:[:created_at, :updated_at]
             end
         else
-            render json: {message: "Trainer not found!"}  
+            render json: {message: "Trainer not found!"}
         end
-    end
+    end 
 end
