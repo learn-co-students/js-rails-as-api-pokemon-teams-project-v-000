8ac5158e66d3b8b06148f9c4d245b7c30054dd7c
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2020-11-18 14:56:35 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -55,12 +55,41 @@ const renderPokemon = (pokemon) => {
 }
 
 
-const createPokemon = () => {
+const createPokemon = (e) => {
+	e.preventDefault()
+	const configObj = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		}, 
+		body: JSON.stringify({trainer_id: e.target.dataset.trainerId})
+	}
 
-}
+	fetch(POKEMONS_URL, configObj)
+	.then(res => res.json())
+	.then(json =>  {
+		if (json.message) {
+			alert(json.message)
+	    } else {
+		  renderPokemon(json)
+    	}
+	})
+		
+ }
 
-const deletePokemon = () => {
-	
+const deletePokemon = (e) => {
+	e.preventDefault()
+    const configObj = {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		}, 
+	}
+
+	fetch(`${POKEMONS_URL}/${e.target.dataset.pokemonId}`, configObj)
+    e.target.parentElement.remove()
 }
 
 
