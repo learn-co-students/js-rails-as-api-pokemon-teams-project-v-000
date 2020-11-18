e68478a873d096490044f6a5da8233cb0b2f8438
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2020-11-18 11:23:56 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,12 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+function ready() {
+fetch(TRAINERS_URL)
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
