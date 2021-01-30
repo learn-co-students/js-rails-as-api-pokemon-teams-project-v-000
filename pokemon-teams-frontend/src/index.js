099ac1b80fd2f926519f3c3b7487add6cfad2962
yurigurlie207 <adrienne.yao@gmail.com>
2021-01-30 15:13:24 -0500

    Added js file to include display trainers and display pokemon functions)

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,28 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", function() {
+ 
+  loadAllTrainers();
+    
+  });
+
+ function loadAllTrainers() {
+   fetch(TRAINERS_URL)
+   .then(res => res.json())
+   .then(results => {
+      results.data.forEach(displayTrainers)
+   });
+ }
+
+ function displayTrainers(value) {
+  // console.log(value.attributes.name)
+  value.attributes.pokemons.forEach(displayPokemons)
+ }
+
+ function displayPokemons(value){
+  // console.log(value.species)
+  // console.log(value.nickname)
+ }
+ 
