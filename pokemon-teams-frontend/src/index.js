b3394d304773aa2a50c5b6e77700273b9a8df31f
yurigurlie207 <adrienne.yao@gmail.com>
2021-01-30 22:18:11 -0500

    Added to call back functions for tainers and pokemon

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,6 +1,7 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const MAIN = document.querySelector('main')
 
 document.addEventListener("DOMContentLoaded", function() {
  
@@ -17,13 +18,30 @@ document.addEventListener("DOMContentLoaded", function() {
  }
 
  function displayTrainers(value) {
-  // console.log(value.attributes.name)
-  value.attributes.pokemons.forEach(displayPokemons)
+    // console.log(value.attributes.name)
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    let ul = document.createElement('ul');
+
+    MAIN.appendChild(div);
+    div.appendChild(p);
+    div.appendChild(ul);
+    div.classList.add("card");
+    p.innerText = value.attributes.name;
+
+    value.attributes.pokemons.forEach(element => displayPokemons(element, ul));
  }
 
- function displayPokemons(value){
-  // console.log(value.species)
-  // console.log(value.nickname)
-  //test git
- }
+
+ function displayPokemons(element, ul) {
+    let li = document.createElement('li');
+    let species = element.species;
+    let nickname = element.nickname;
+    li.innerText = species.concat(" ( ", nickname, " )");
+    ul.appendChild(li);
+  }
+
+
+ 
+
  
