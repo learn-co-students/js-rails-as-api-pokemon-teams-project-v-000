b98881f47c6b26bbe57e7b51503ed8fcbaa5be1e
klovae <elyse.klova@gmail.com>
2021-04-22 11:01:58 -0400

    set up backend to prep for project

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,9 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+// When a user loads the page, they should see all trainers, with their current team of Pokemon.
+
+// Whenever a user hits "Add Pokemon" and they have space on their team, they should get a new Pokemon.
+
+// Whenever a user hits "Release Pokemon" on a specific Pokemon team, that specific Pokemon should be released from the team.
